b1a5cdf52cbd58525ecd19bd48240668d7b39fa8
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2023-04-22 19:15:29 +0700

    feat: create register

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -6,18 +6,18 @@
         <div class="w-full flex flex-col items-center gap-y-3">
           <div class="flex flex-col w-full">
             <label class="text-slate-600" for="username">Email</label>
-            <input type="email" name="email" id="email" class="border rounded-md w-full py-1 px-1" />
+            <input type="email" name="email" id="email" class="border rounded-md w-full py-1 px-1"  v-model="data.email"/>
           </div>
           <div class="flex flex-col w-full">
             <label class="text-slate-600" for="username">Password</label>
-            <input type="password" name="password" id="password" class="border rounded-md w-full py-1 px-1" />
+            <input type="password" name="password" id="password" class="border rounded-md w-full py-1 px-1"  v-model="data.password"/>
           </div>
           <div class="flex flex-col w-full">
             <label class="text-slate-600" for="username">Confirm Password</label>
-            <input type="password" name="confirmPassword" id="confirmPassword" class="border rounded-md w-full py-1 px-1" />
+            <input type="password" name="confirmPassword" id="confirmPassword" class="border rounded-md w-full py-1 px-1"  v-model="data.confirmPassword"/>
           </div>
         </div>
-        <button class="bg-[#EB6648] text-white w-full rounded-md py-1.5">submit</button>
+        <button @click="register" class="bg-[#EB6648] text-white w-full rounded-md py-1.5">submit</button>
         <p class="text-sm xl:text-md text-gray-400 text-center mt-20">
           Have an account? <a @click="this.$router.push('/login')" class="text-black/70 cursor-pointer">Back to login ✨</a>
         </p>
@@ -34,9 +34,23 @@ export default {
   },
   data() {
     return {
-      msg: "Hello World",
+      data: {
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
     };
   },
   onMounted() {},
+  methods: {
+    async register() {
+      try {
+        await axios.post("http://localhost:8080/api/user/register", this.data);
+        this.$router.push("/login");
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    },
+  },
 };
 </script>
