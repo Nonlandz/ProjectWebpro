06ad5e6583127e84676d47c740f902215bc07998
Nonlandz <nonlandza123@gmail.com>
2023-04-26 13:38:20 +0700

    Update Footer.vue

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,3 +1,3 @@
 <template>
-  <footer class="py-1 w-full shadow text-center bg-[#1E4F79] text-white">@2021</footer>
+  <footer class="py-1 w-full shadow text-center bg-[#1E4F79] text-white">@ITxChange2023</footer>
 </template>
