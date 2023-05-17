e8d69273ae4dfe3c622230d5ce0921df4fd07f77
Nonlandz <88500373+Nonlandz@users.noreply.github.com>
2023-05-18 00:56:48 +0700

    info

--- client/src/components/userinfo.vue
+++ client/src/components/userinfo.vue
@@ -1,40 +1,55 @@
 <template>
-    <div class="userinfo-form min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md w-full space-y-8">
-        <form @submit.prevent="submitForm" class="mt-8 space-y-6">
+    <div class="userinfo-form min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-[#f4f1f1]">
+      <div class="w-6/12 py-14 justify-center items-center content-center flex flex-col rounded-md shadow-md bg-white ">
+        <h1 class="text-4xl text-slate-600 font-bold mb-5">Profile</h1>
+      <div class="max-w-md w-full space-y-5">
+        <form @submit.prevent="submitForm" class="">
           <input type="hidden" name="remember" value="true">
           <div class="rounded-md shadow-sm -space-y-px">
             <div>
+              <!-- <label for="username" class="sr-only">Username</label>
+              <input id="username" name="username" type="text" v-model="form.username" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Username"> -->
               <label for="username" class="sr-only">Username</label>
-              <input id="username" name="username" type="text" v-model="form.username" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Username">
+              <input id="username" name="username" type="text" class="border rounded-md w-full py-1 px-1 mb-3 h-9" v-model="form.username" placeholder="Username"/>
             </div>
             <div>
+              <!-- <label for="firstName" class="sr-only">First Name</label>
+              <input id="firstName" name="firstName" type="text" v-model="form.firstName" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="First Name"> -->
               <label for="firstName" class="sr-only">First Name</label>
-              <input id="firstName" name="firstName" type="text" v-model="form.firstName" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="First Name">
+              <input id="firstName" name="firstName" type="text" class="border rounded-md w-full py-1 px-1 mb-3 h-9" v-model="form.firstName" placeholder="First Name"/>
             </div>
             <div>
-              <label for="lastName" class="sr-only">Last Name</label>
-              <input id="lastName" name="lastName" type="text" v-model="form.lastName" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Last Name">
+              <!-- <label for="lastName" class="sr-only">Last Name</label>
+              <input id="lastName" name="lastName" type="text" v-model="form.lastName" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Last Name"> -->
+              <label for="lastName" class="sr-only">First Name</label>
+              <input id="lastName" name="lastName" type="text" class="border rounded-md w-full py-1 px-1 mb-3 h-10" v-model="form.lastName" placeholder="Last Name"/>
+              
             </div>
             <div>
-              <label for="phone" class="sr-only">Phone</label>
-              <input id="phone" name="phone" type="text" v-model="form.phone" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Phone">
+              <!-- <label for="phone" class="sr-only">Phone</label>
+              <input id="phone" name="phone" type="text" v-model="form.phone" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Phone"> -->
+              <label for="phone" class="sr-only">First Name</label>
+              <input id="phone" name="phone" type="text" class="border rounded-md w-full py-1 px-1 mb-3 h-10" v-model="form.phone" placeholder="Phone Number"/>
             </div>
             <div>
+              <!-- <label for="address" class="sr-only">Address</label>
+              <input id="address" name="address" type="text" v-model="form.address" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Address"> -->
               <label for="address" class="sr-only">Address</label>
-              <input id="address" name="address" type="text" v-model="form.address" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-200 focus:border-blue-200 focus:z-10 sm:text-sm" placeholder="Address">
+              <input id="address" name="address" type="text" class="border rounded-md w-full py-1 px-1 mb-7 h-10" v-model="form.address" placeholder="Address"/>
             </div>
           </div>
           <div>
-            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-200">
+            <button type="submit" class="bg-[#EB6648] group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:bg-[#df593a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-200">
               Submit
             </button>
+            <!-- <button @click="login" class="bg-[#EB6648] text-white w-full rounded-md py-1.5">Submit</button> -->
           </div>
         </form>
-        <button @click="goBackToLogin" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-200">
+        <button @click="goBackToLogin" class="bg-[#286aa2] group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white hover:bg-[#1a456a]  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-200">
       Back to Login
     </button>
     </div>
+  </div>
   </div>
 </template>
 
@@ -134,4 +149,10 @@ async submitForm() {
 }
 </script>
 
+
+<style>
+
+
+
+</style>
   
